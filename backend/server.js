d838d1642e3c82035c8a4184c5ff8d1c9a7963a6
Rahul George <rahuljithu911@gmail.com>
2024-09-22 19:26:58 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
