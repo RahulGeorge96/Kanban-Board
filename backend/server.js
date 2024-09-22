2695c14164abfb952587047a07df054123b2ba94
Rahul George <rahuljithu911@gmail.com>
2024-09-22 19:32:25 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173"
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
